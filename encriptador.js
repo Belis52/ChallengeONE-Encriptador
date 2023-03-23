04de61d08616ec7dec0fc415c7f9aad72eb365bd
Isabel <isabel@gmail.com>
2023-03-23 18:57:10 -0500

    Agregando archivos del Challenge Encriptador

---
+++ encriptador.js
@@ -0,0 +1,63 @@
+const textoIngresado = document.querySelector(".areaEncriptacion-entrada__textoIngresado");
+const textoResultante = document.querySelector(".areaEncriptacion-salida__textoResultante");
+const imagenRobot = document.querySelector(".areaEncriptacion-salida__robot");
+const areaCopia = document.querySelector(".areaEncriptacion-salida__copia");
+const notas = document.querySelector(".areaEncriptacion-salida__notas");
+
+function validar(texto) {
+    var tecla = texto.charCode || texto.which;
+    if (!((tecla >= 97 && tecla <= 122) || tecla == 241)){
+        alert("Solo se aceptan letras minúsculas y sin acentos");
+        return false
+    }
+}
+
+function botonEncriptar(){
+    const encriptado = encriptar(textoIngresado.value);
+    textoResultante.value = encriptado;
+    textoIngresado.value ="";
+    imagenRobot.style.display = "none";
+    notas.style.display = "none";
+    textoResultante.style.display ="block";
+    areaCopia.style.display ="block"
+}
+
+function botonDesencriptar(){
+    const desencriptado = desencriptar(textoIngresado.value);
+    textoResultante.value = desencriptado;
+    textoIngresado.value ="";
+}
+
+function botonCopiar(){
+    copiar(textoResultante.value);
+}
+
+function encriptar(texto){
+    let llavesEncriptacion = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < llavesEncriptacion.length; i++){
+        if(texto.includes(llavesEncriptacion[i][0])){
+            texto = texto.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1]);
+        }
+    }
+    return texto;
+}
+
+function desencriptar(texto){
+    let llavesEncriptacion = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < llavesEncriptacion.length; i++){
+        if(texto.includes(llavesEncriptacion[i][1])){
+            texto = texto.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0]);
+        }
+    }
+    return texto;        
+}
+
+function copiar(){
+    var textoCopiado = document.querySelector(".areaEncriptacion-salida__textoResultante");
+    textoCopiado.select();
+    navigator.clipboard.writeText(textoCopiado.value);
+}
